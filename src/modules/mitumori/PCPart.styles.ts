ee1166f2d88a604b56ff5d8eaa77b988054787d1
Nomura-Kota <nomura.kota2005@gmail.com>
2024-01-16 22:47:09 +0900

    test

---
+++ src/modules/mitumori/PCPart.styles.ts
@@ -0,0 +1,13 @@
+import { SxProps, Theme } from '@mui/material';
+
+export const modalStyle: SxProps<Theme> = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
---
+++ src/modules/mitumori/PCPart.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Box, Typography, Button, Modal } from '@mui/material';
+import { modalStyle } from './PCPart.styles';
+import { PartDetail, PCPartProps } from './types';
+import { PCPartsData } from './PCPartsData';
+
+  // 個々のPC部品を選択するためのコンポーネント
+  export const PCPart = ({ partName, onSelectPart }: PCPartProps & { onSelectPart: (part: PartDetail) => void }) => {
+    // モーダルの開閉状態を管理するための状態
+    const [isModalOpen, setModalOpen] = useState(false);
+    // 選択された部品を管理するための状態
+    const [selectedPart, setSelectedPart] = useState<PartDetail | null>(null);
+  
+    // モーダルを開くための関数
+    const openModal = () => {
+      setModalOpen(true);
+    };
+  
+    // モーダルを閉じるための関数
+    const closeModal = () => {
+      setModalOpen(false);
+    };
+  
+    // 部品を選択するための関数。選択された部品を状態に設定し、親コンポーネントに通知
+    const selectPart = (part: PartDetail) => {
+      setSelectedPart(part);
+      onSelectPart(part);
+      closeModal();
+    };
+  
+    return (
+      <>
+        <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
+          <Typography variant="subtitle1">{partName}</Typography>
+          <Typography variant="subtitle1">
+            {selectedPart ? `${selectedPart.price}円` : '0円'}
+          </Typography>
+          <Button variant="outlined" onClick={openModal}>
+            パーツを選択してください
+          </Button>
+        </Box>
+  
+        <Modal open={isModalOpen} onClose={closeModal}>
+          <Box sx={modalStyle}>
+            {/* 利用可能な部品のリストを表示し、選択可能にする */}
+            {PCPartsData[partName].map((part, index) => (
+              <Box key={index} display="flex" justifyContent="space-between" alignItems="center" marginBottom={2}>
+                <Typography variant="subtitle1">
+                  {part.name} - {part.price}円
+                </Typography>
+                <Button variant="outlined" onClick={() => selectPart(part)}>
+                  選択
+                </Button>
+              </Box>
+            ))}
+          </Box>
+        </Modal>
+      </>
+    );
+  };
+  
